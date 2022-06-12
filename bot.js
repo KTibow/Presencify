055c1afbc3df435fd8bd0d8f0842145020344ab0
Kendell R <KTibow@users.noreply.github.com>
2022-06-11 18:06:37 -0700

    revamp

--- bot.js
+++ bot.js
@@ -42,67 +42,28 @@ client.on("messageCreate", async (message) => {
     await fs.writeFile("data/optOut.json", JSON.stringify(optOut));
   }
   if (optOut.includes(message.author.id)) return;
-  // Update anything currently tracking
-  const currentlyTracking = await fetchJson("data/tracking.json", []);
   const usersData = await fetchJson("data/users.json", {});
-  currentlyTracking.forEach((trackData) => {
-    if (trackData.id == message.author.id) {
-      const currentUserData = usersData[trackData.id] || [];
-      if (Date.now() - trackData.startedAt > 10 * 60 * 1000) {
-        currentUserData.push({
-          userData: trackData.userData,
-          responded: false,
-        });
-      } else {
-        currentUserData.push({
-          userData: trackData.userData,
-          responded: true,
-        });
-      }
-      currentlyTracking.remove(trackData);
-      usersData[trackData.id] = currentUserData;
-    }
-  });
-  // Record for any new mentions
-  const lastMessages = await fetchJson("data/lastMessages.json", {});
-  const mentions = Array.from(message.mentions.users.values());
-  if (message.mentions.repliedUser) {
-    mentions.push(message.mentions.repliedUser);
+  if (!usersData[message.author.id]) {
+    usersData[message.author.id] = { messageTimes: [] };
   }
-  mentions.forEach((mention) => {
-    if (optOut.includes(mention.id)) return;
-    // Check if the user is already being tracked
-    if (currentlyTracking.find((trackData) => trackData.id == mention.id)) return;
-    // Fetch info about the mentioned user
-    const guildMemberMentioned = client.guilds.cache
-      .get(message.guild.id)
-      .members.cache.get(mention.id);
-    const userData = {
-      presence: guildMemberMentioned.presence.status,
-      sinceLastMessage:
-        lastMessages[mention.id] && new Date() - lastMessages[mention.id],
-      utcHour: new Date().getUTCHours(),
-      utcDay: new Date().getUTCDay(),
-    };
-    // Add the user to the tracking list
-    currentlyTracking.push({
-      id: mention.id,
-      userData,
-      startedAt: Date.now(),
-    });
-  });
-  // Record the last message sent by the user
-  lastMessages[message.author.id] = Date.now();
+  usersData[message.author.id].messageTimes.push(message.createdAt.getTime());
 
   await fs.writeFile("data/users.json", JSON.stringify(usersData));
-  await fs.writeFile("data/tracking.json", JSON.stringify(currentlyTracking));
-  await fs.writeFile("data/lastMessages.json", JSON.stringify(lastMessages));
-  await fs.writeFile("data/optOut.json", JSON.stringify(optOut));
 });
-/*const calculateIfLikelyOnline = async (user) => {
-  const userDataList = await fetchJson("data/users.json", {});
-  const userData = userDataList.find((user) => user.id == user.id);
-  if (!user) return;
-};*/
+const calculateRespProb = (
+  timesResponded,
+  totalTimesResponded,
+  timesNotResponded,
+  totalTimesNotResponded
+) => {
+  return (
+    timesResponded /
+    totalTimesResponded /
+    (timesResponded / totalTimesResponded + timesNotResponded / totalTimesNotResponded)
+  );
+};
+const calculateIfLikelyOnline = async (user) => {
+  // TODO
+};
 console.log("loaded");
 client.login();
